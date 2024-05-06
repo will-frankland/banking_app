e7869cb8b403269a1f6f79290b8367af9d49e50d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-06 19:14:30 -0400

    map sidebar links and style with dynamic classNames utilizing pathname

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Will', lastName: "Frankland"}
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn}/>
       {children}
     </main>
   );
