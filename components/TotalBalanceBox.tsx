fbf51a5ddf04437ab432cc3678f9363211c8c6be
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-06 19:04:57 -0400

    implement doughnutChart with mock data

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,6 +1,6 @@
 import { formatAmount } from "@/lib/utils";
-import React from "react";
 import AnimatedCounter from "./AnimatedCounter";
+import DoughnutChart from "./DoughnutChart";
 
 const TotalBalanceBox = ({
   accounts = [],
@@ -9,13 +9,16 @@ const TotalBalanceBox = ({
 }: TotlaBalanceBoxProps) => {
   return (
     <section className="total-balance">
-      <div className="total-balance-chart">{/* DoughnutChart */}</div>
+      <div className="total-balance-chart">
+        <DoughnutChart accounts={accounts} />
+      </div>
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
-          <p className="total-balance-amount flex-center gap-2">
-            <AnimatedCounter amount={totalCurrentBalance} /></p>
+          <div className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount={totalCurrentBalance} />
+          </div>
         </div>
       </div>
     </section>
