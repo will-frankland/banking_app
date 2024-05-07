720ee23b181baa3ba0fe05770a7548367bdb97ec
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-06 22:27:12 -0400

    create mobileNav component and style

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+        <Link href="/" className="mb-12 flex cursor-pointer items-center gap-2">
           <Image
             src="/icons/logo.svg"
             width={34}
@@ -25,11 +25,21 @@ const Sidebar = ({ user }: SiderbarProps) => {
           const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
           return (
             <Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}>
-              {item.label}
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({'brightness-[3] invert-0': isActive})}
+                />
+              </div>
+              <p className={cn('sidebar-label', {'!text-white': isActive})}>{item.label}</p>
             </Link>
           );
         })}
+        USER
       </nav>
+      FOOTER
     </section>
   );
 };
