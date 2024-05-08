50c83190b9e81a1c86e2c561327b2e90ca5982f0
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-08 12:26:56 -0400

    setup signin functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,50 +1,55 @@
-"use server"
+"use server";
 
-import { ID } from "node-appwrite"
-import { createAdminClient, createSessionClient } from "../appwrite"
-import { cookies } from "next/headers"
-import { parseStringify } from "../utils"
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
 
-export const signIn = async () => {
+export const signIn = async ({ email, password }: signInProps) => {
   try {
-    // Mutation / Database / Make fetch
-  } catch (error) {
-    console.error('Error', error)
-  }
-}
+    const { account } = await createAdminClient();
 
-export const signUp = async ( userData: SignUpParams ) => {
+    const response = await account.createEmailPasswordSession(email, password)
+
+    return parseStringify(response);
+
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
   const { email, password, firstName, lastName } = userData;
   try {
     const { account } = await createAdminClient();
 
-  const newUserAccount = await account.create(
-    ID.unique(),
-    email,
-    password,
-    `${firstName} ${lastName}`
-  );
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
 
-  const session = await account.createEmailPasswordSession(email, password);
+    const session = await account.createEmailPasswordSession(email, password);
 
-  cookies().set("appwrite-session", session.secret, {
-    path: "/",
-    httpOnly: true,
-    sameSite: "strict",
-    secure: true,
-  });
-  return parseStringify(newUserAccount);
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    return parseStringify(newUserAccount);
   } catch (error) {
-    console.error('Error', error)
+    console.error("Error", error);
   }
-}
+};
 
 export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
     const user = await account.get();
 
-    return parseStringify(user)
+    return parseStringify(user);
   } catch (error) {
     return null;
   }
