33562be60a7442ecde06aeb9f63cd54ec4ab55c7
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-08 10:53:48 -0400

    add loading state and animation alongside disabled when clicked to sign-in form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,9 +20,11 @@ import {
 import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof authFormSchema>>({
@@ -37,7 +39,9 @@ const AuthForm = ({ type }: { type: string }) => {
   function onSubmit(values: z.infer<typeof authFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setIsLoading(true);
     console.log(values);
+    setIsLoading(false);
   }
 
   return (
@@ -84,7 +88,13 @@ const AuthForm = ({ type }: { type: string }) => {
                 placeholder={"Enter your password"}
               />
 
-              <Button type="submit">Submit</Button>
+              <Button type="submit" className="form-btn" disabled={isLoading}>
+                {isLoading ? (
+                  <>
+                    <Loader2 size={20} className="animate-spin"/> &nbsp; Loading...
+                  </>
+                ) : type === 'sign-in' ? 'Sign In' : "Sign Up"}
+              </Button>
             </form>
           </Form>
         </>
