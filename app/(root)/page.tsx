49a205671de59bef726923b6e260445a8a1c6552
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-10 14:10:58 -0400

    add recent transactions and bank item tab files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,32 +1,31 @@
-import HeaderBox from '@/components/HeaderBox'
-import RightSidebar from '@/components/RightSidebar'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-import { getAccount, getAccounts } from '@/lib/actions/bank.actions'
-import { getLoggedInUser } from '@/lib/actions/user.actions'
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import React from "react";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+
+  const currentPage = Number(page as string) || 1;
+  
   const loggedIn = await getLoggedInUser();
 
-  const accounts = await getAccounts({ userId: loggedIn.$id })
+  const accounts = await getAccounts({ userId: loggedIn.$id });
 
-  if(!accounts) return;
+  if (!accounts) return;
 
-  const accountsData = accounts?.data
+  const accountsData = accounts?.data;
 
-  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  const account = await getAccount({ appwriteItemId })
-
-  console.log({
-    accountsData,
-    account
-  })
+  const account = await getAccount({ appwriteItemId });
 
   return (
-    <section className='home'>
-      <div className='home-content'>
-        <header className='home-header'>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
           <HeaderBox
             type="greeting"
             title="Welcome"
@@ -39,11 +38,20 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
             totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
-        RECENT TRANSACTIONS
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
-      <RightSidebar user={loggedIn} transactions={accounts?.transactions} banks={accountsData?.slice(0, 2)}/>
+      <RightSidebar
+        user={loggedIn}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
