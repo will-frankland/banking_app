8236299821060e30eaea63045854c51652c3b1c1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-10 14:56:45 -0400

    add cells with styling to table

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -7,25 +7,87 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { transactionCategoryStyles } from "@/constants";
+import {
+  cn,
+  formatAmount,
+  formatDateTime,
+  getTransactionStatus,
+  removeSpecialCharacters,
+} from "@/lib/utils";
 
-const TransactionsTable = () => {
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+  const { borderColor, backgroundColor, textColor, chipBackgroundColor } =
+    transactionCategoryStyles[
+      category as keyof typeof transactionCategoryStyles
+    ] || transactionCategoryStyles.default;
+  return (
+    <div className={cn("category-badge", borderColor, chipBackgroundColor)}>
+      <div className={cn("size-2 rounded-full", backgroundColor)} />
+      <p className={cn("text-[12] font-medium", textColor)}>{category}</p>
+    </div>
+  );
+};
+
+const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return (
     <Table>
-      <TableHeader>
+      <TableHeader className="bg-[#f9fafb]">
         <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
+          <TableHead className="px-2">Transaction</TableHead>
+          <TableHead className="px-2">Amount</TableHead>
+          <TableHead className="px-2">Status</TableHead>
+          <TableHead className="px-2">Date</TableHead>
+          <TableHead className="px-2 max-md:hidden">Channel</TableHead>
+          <TableHead className="px-2 max-md:hidden">Category</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">INV001</TableCell>
-          <TableCell>Paid</TableCell>
-          <TableCell>Credit Card</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
+        {transactions.map((transaction: Transaction) => {
+          const status = getTransactionStatus(new Date(transaction.date));
+          const amount = formatAmount(transaction.amount);
+
+          const isDebit = transaction.type === "debit";
+          const isCredit = transaction.type === "credit";
+
+          return (
+            <TableRow
+              key={transaction.id}
+              className={`${
+                isDebit || amount[0] === "-" ? "bg-[#FFFBFA]" : "bg-[#F6FEF9]"
+              } !over:bg-none !border-b-DEFAULT`}
+            >
+              <TableCell className="max-w-[250px] pl-2 pr-10">
+                <div className="flex items-center gap-3">
+                  <h1 className="text-14 truncate font-semibold text-[#344054]">
+                    {removeSpecialCharacters(transaction.name)}
+                  </h1>
+                </div>
+              </TableCell>
+              <TableCell
+                className={`pl-2 pr-10 font-semibold ${
+                  isDebit || amount[0] === "-"
+                    ? "text-[#f04438]"
+                    : "text-[#039855]"
+                }`}
+              >
+                {isDebit ? `-${amount}` : isCredit ? amount : amount}
+              </TableCell>
+              <TableCell className="pl-2 pr-10">
+                <CategoryBadge category={status} />
+              </TableCell>
+              <TableCell className="min-w-32 pl-2 pr-10">
+                {formatDateTime(new Date(transaction.date)).dateTime}
+              </TableCell>
+              <TableCell className="pl-2 pr-10 capitalize min-w-24">
+                {transaction.paymentChannel}
+              </TableCell>
+              <TableCell className="pl-2 pr-10 max-md:hidden">
+                <CategoryBadge category={transaction.category} />
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
