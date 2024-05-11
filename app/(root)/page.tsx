b9942fac8a8cdfbe41eb73b9c18a7da9515d69cd
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-05-10 20:34:58 -0400

    implement transaction history page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,9 +7,8 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
-
   const currentPage = Number(page as string) || 1;
-  
+
   const loggedIn = await getLoggedInUser();
 
   const accounts = await getAccounts({ userId: loggedIn.$id });
